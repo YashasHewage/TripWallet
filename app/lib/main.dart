60847a18fb5e4a33b14eb3ca5dcc9a5619d543fa
Dannansilva <dannangunathilaka@gmail.com>
2023-11-03 22:05:29 +0530

    all flutter pages

---
+++ app/lib/main.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
