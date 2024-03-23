b79a33fecdee4f52bc038dac0df847e1da89745b
YashasHewage <yashrashh05@gmail.com>
2024-03-24 01:25:57 +0530

    profile changes

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,11 +3,15 @@
 import 'package:app/pages/Expences_page.dart';
 import 'package:app/pages/default_addtrip.dart';
 import 'package:app/pages/home_page.dart';
+import 'package:app/pages/language_set.dart';
 import 'package:app/pages/login_page.dart';
+import 'package:app/pages/profile_edit.dart';
+import 'package:app/pages/profilepage.dart';
 import 'package:app/pages/signup_con_page.dart';
 import 'package:app/pages/signup_per_page.dart';
 import 'package:app/widgets/all_expences_page.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -23,13 +27,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (context) => const Addtrip(),
+        '/settings': (context) => const ProfilePage(),
         '/homepage': (context) => const HomePage(),
         '/expencespage': (context) => const ExpencesState(),
         '/allexpences': (context) => const AllExpences(),
         '/login': (context) => const LoginPage(),
         '/person': (context) => const Signupperinfo(),
         '/concat': (context) => const Signupcon(),
-      },
+        '/profileEdit': (context) => const ProfileEdit(), 
+        '/languages': (context) => const LanguageSettingsPage(),
+      }
     );
   }
 }
