19528726565ca3a30b6a89ccad74ee34274b5079
Navishh <navishkanuwan11@gmail.com>
2024-03-24 22:01:36 +0530

    Updated Profile

--- app/lib/pages/profilepage.dart
+++ app/lib/pages/profilepage.dart
@@ -3,42 +3,42 @@ import 'package:app/widgets/settingsWidgets/profile_header.dart';
 import 'package:app/widgets/settingsWidgets/ProfileMenuOptions.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.grey[200],
-      child: Column(
-        children: [
-          const ProfileHeader(),
-          const ProfileMenuOptions(),
-          const SizedBox(
-              height:
-                  20), // Add space between the ProfileMenuOptions and the Login button
-          ElevatedButton(
-            onPressed: () {
-              // Add your login function here
-            },
-            child: Text(
-              'Logout',
-              style: const TextStyle(fontSize: 20,color: Colors.white),
-              
+      child: SingleChildScrollView( // Added SingleChildScrollView
+        child: Column(
+          children: [
+            const ProfileHeader(),
+            const ProfileMenuOptions(),
+            const SizedBox(height: 20), 
+            Container(
+              margin: const EdgeInsets.only(left: 20, right:20, bottom: 20), 
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                child: Text(
+                  'Logout',
+                  style: const TextStyle(fontSize: 20,color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:const Color.fromRGBO(249, 73, 73, 1), 
+                  textStyle: const TextStyle(
+                      color: Colors.white), 
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10),
+                  ),
+                  minimumSize: const Size(385, 60),
+                  alignment: Alignment.center, 
+                ),
               ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor:const Color.fromRGBO(249, 73, 73, 1), // Set the button color as needed
-              textStyle: const TextStyle(
-                  color: Colors.white), // Set the text color as needed
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                    10), // Set the border radius as needed
-              ),
-              minimumSize: const Size(385,
-                  60), // Make the button full width and 50 pixels high
-              alignment: Alignment.center, // Center the text in the button
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
