c54d2badf7f450a117566fdd381dafae36cc1d05
YashasHewage <yashrashh05@gmail.com>
2024-03-28 04:24:24 +0530

    changes

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
-
 import 'package:google_fonts/google_fonts.dart';
-
 import 'package:app/components/my_textfield.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -17,30 +14,15 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  bool isLoading = false;
-
   Future<void> login() async {
-    setState(() {
-      isLoading = true;
-    });
-    
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: emailController.text.trim(),
           password: passwordController.text.trim());
     } catch (e) {
-      showDialog(
-        // ignore: use_build_context_synchronously
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text(e.toString()),
-        ),
-      );
-
+      print(e);
+      return;
     }
-    setState(() {
-      isLoading = false;
-    });
     if (mounted) {
       Navigator.pushNamed(context, '/addtrip');
     }
@@ -48,34 +30,83 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: isLoading
-            ? Center(child: CircularProgressIndicator())
-            : SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 18),
+                  // Top text
+
+                  Container(
+                    margin: EdgeInsets.only(top: 40.0, left: 10),
+                    child: Text(
+                      'Welcome Back!',
+                      style: GoogleFonts.poppins(
+                          fontSize: 24, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Container(
+                    margin: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'Few steps away',
+                      style: GoogleFonts.poppins(),
+                    ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0, bottom: 15.0),
+                    child: Center(
+                      child: Container(
+                        width: 300,
+                        height: 300,
+                        child: Image.asset('assets/login.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  // Text fields
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(height: 18),
-                      // Top text
                       Container(
-                        margin: EdgeInsets.only(top: 40.0, left: 10),
-                        child: Text(
-                          'Welcome Back!',
-                          style: GoogleFonts.poppins(
-                              fontSize: 24, fontWeight: FontWeight.w600),
+                        height: 50,
+                        child: MyTextField(
+                          controller: emailController,
+                          hintText: 'Email',
+                          style: GoogleFonts.poppins(),
+                          obscureText: false,
                         ),
                       ),
-                      SizedBox(height: 6),
+                      SizedBox(height: 20),
                       Container(
-                        margin: EdgeInsets.symmetric(horizontal: 15.0), 
+                        height: 50, // Change this value to your desired height
+                        child: MyTextField(
+                          controller: passwordController,
+                          hintText: 'Password',
+                          style: GoogleFonts.poppins(),
+                          obscureText: true,
+                        ),
+                      ),
+
+                      SizedBox(height: 40),
+
+                      // Login button
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 15.0),
                         child: ElevatedButton(
                           onPressed: () {
                             login();
-                          }, 
+                          },
                           style: ButtonStyle(
                             shape: MaterialStateProperty.all(
                               RoundedRectangleBorder(
@@ -92,101 +123,44 @@ class _LoginPageState extends State<LoginPage> {
                           child: Text(
                             "Login",
                             style: GoogleFonts.poppins(
-                              color: Colors.white, 
-                              fontSize: 18
-                            ),
-
+                                color: Colors.white, fontSize: 18),
                           ),
                         ),
                       ),
-                      SizedBox(height: 10),
-                      // Text fields
-                      Column(
+                      SizedBox(height: 2),
+
+                      // Don't have an account? signup
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Container(
-                            height: 50,
-                            child: MyTextField(
-                              controller: emailController,
-                              hintText: 'Email',
-                              style: GoogleFonts.poppins(),
-                              obscureText: false,
-                            ),
+                          Text(
+                            "Don't have an account?",
+                            style: GoogleFonts.poppins(fontSize: 14),
                           ),
-                          SizedBox(height: 20),
-                          Container(
-                            height:
-                                50, // Change this value to your desired height
-                            child: MyTextField(
-                              controller: passwordController,
-                              hintText: 'Password',
-                              style: GoogleFonts.poppins(),
-                              obscureText: true,
-                            ),
+                          SizedBox(
+                            width: 5,
                           ),
-
-                          SizedBox(height: 40),
-
-                          // Login button
-                          Container(
-                            margin: EdgeInsets.symmetric(horizontal: 15.0),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                login();
-                              }, // Add your login functionality here
-                              style: ButtonStyle(
-                                shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                                minimumSize: MaterialStateProperty.all(
-                                  ui.Size(350, 60),
-                                ),
-                                backgroundColor: MaterialStateProperty.all(
-                                  Color(0xFF213660),
-                                ),
-                              ),
-                              child: Text(
-                                "Login",
-                                style: GoogleFonts.poppins(
-                                    color: Colors.white, fontSize: 18),
-                              ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/concat');
+                            },
+                            child: Text(
+                              'Sign up',
+                              style: GoogleFonts.poppins(
+                                  color: Colors.blue,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400),
                             ),
-                          ),
-                          SizedBox(height: 2),
-
-                          // Don't have an account? signup
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Don't have an account?",
-                                style: GoogleFonts.poppins(fontSize: 14),
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/concat');
-                                },
-                                child: Text(
-                                  'Sign up',
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.blue,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400),
-                                ),
-                              ),
-                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
+            ),
+          ),
+        ),
       ),
     );
   }
